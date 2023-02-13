521a3fb5188cd46072b3f4594f9ffd54d7d49108
Do11a-Bill <billym41620@gmail.com>
2023-02-12 17:46:08 -0800

    Javadoc finished on Game class

--- GameModule/src/main/Game.java
+++ GameModule/src/main/Game.java
@@ -2,20 +2,30 @@ package main;
 
 public class Game implements Runnable{
 
-    //The frame or the borders surrounding the panel.
+    /**
+     * Our Window, Frame, or borders surrounding the panel
+     */
     private GameWindow myGameWindow;
 
-    //What's shown or made. Imagine it like a canvas that we're painting on.
+    /**
+     * Our panel; what's shown inside our GameWindow. Imagine it like a canvas we're painting on
+     */
     private GamePanel myGamePanel;
 
-    //Allows us to do multiple things at once. Like Having things run in the background,
-    //Without interrupting the main program.
+    /**
+     * Allows us to do multiple things at once. Like Having things run in the background,
+     * without interrupting the main program.
+     */
     private Thread myGameThread;
 
-    //Capping the FPS at 120
+    /**
+     * Capping the FPS to 120.
+     */
     private final int FPS_SET = 120;
 
-    //Constructor, Called in the main Class. Initializes everything we need.
+    /**
+     * Initializes everything we need. Called in the main Class.
+     */
     public Game() {
         myGamePanel = new GamePanel();
         myGameWindow = new GameWindow(myGamePanel);
@@ -24,13 +34,17 @@ public class Game implements Runnable{
 
     }
 
-    //Initializes and starts the thread. It's what makes our game start.
+    /**
+     * Initializes and starts the thread.
+     */
     private void startGameLoop() {
         myGameThread = new Thread(this);
         myGameThread.start();
     }
 
-    //Runs the thread. Updates everything on the screen
+    /**
+     * Runs the thread. Updates everything on the screen.
+     */
     @Override
     public void run() {
 
